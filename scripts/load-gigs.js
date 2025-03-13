5386dc8a1eb08908a306233de46db951d966f4a2
Arlo Guthrie <arlo@drata.com>
2025-03-12 19:09:12 -0500

    Store gigs in CSV, update copyright

---
+++ scripts/load-gigs.js
@@ -0,0 +1,25 @@
+"use strict";
+const fs = require("fs");
+const upath = require("upath");
+const { parse } = require("csv-parse/sync");
+
+module.exports = function loadGigs() {
+    const gigsPath = upath.resolve(
+        upath.dirname(__filename),
+        "../src/data/gigs.csv"
+    );
+
+    try {
+        const csvContent = fs.readFileSync(gigsPath, "utf-8");
+        const records = parse(csvContent, {
+            columns: true,
+            skip_empty_lines: true,
+        });
+
+        // Sort gigs by date
+        return records.sort((a, b) => new Date(a.date) - new Date(b.date));
+    } catch (err) {
+        console.warn("Warning: Could not load gigs data:", err.message);
+        return [];
+    }
+};
--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -1,34 +1,38 @@
-'use strict';
-const fs = require('fs');
-const upath = require('upath');
-const pug = require('pug');
-const sh = require('shelljs');
-const prettier = require('prettier');
+"use strict";
+const fs = require("fs");
+const upath = require("upath");
+const pug = require("pug");
+const sh = require("shelljs");
+const prettier = require("prettier");
+const loadGigs = require("./load-gigs");
 
 module.exports = function renderPug(filePath) {
-    const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
-    const srcPath = upath.resolve(upath.dirname(__filename), '../src');
+    const destPath = filePath
+        .replace(/src\/pug\//, "dist/")
+        .replace(/\.pug$/, ".html");
+    const srcPath = upath.resolve(upath.dirname(__filename), "../src");
 
     console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
     const html = pug.renderFile(filePath, {
-        doctype: 'html',
+        doctype: "html",
         filename: filePath,
-        basedir: srcPath
+        basedir: srcPath,
+        gigs: loadGigs(), // Load gigs data
     });
 
     const destPathDirname = upath.dirname(destPath);
-    if (!sh.test('-e', destPathDirname)) {
-        sh.mkdir('-p', destPathDirname);
+    if (!sh.test("-e", destPathDirname)) {
+        sh.mkdir("-p", destPathDirname);
     }
 
     const prettified = prettier.format(html, {
         printWidth: 1000,
         tabWidth: 4,
         singleQuote: true,
-        proseWrap: 'preserve',
-        endOfLine: 'lf',
-        parser: 'html',
-        htmlWhitespaceSensitivity: 'ignore'
+        proseWrap: "preserve",
+        endOfLine: "lf",
+        parser: "html",
+        htmlWhitespaceSensitivity: "ignore",
     });
 
     fs.writeFileSync(destPath, prettified);
